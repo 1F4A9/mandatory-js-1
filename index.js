3a20851e3bb8d7f79c3d1ff6c52dbc71ff308abd
Emil Ahlm <emil.ahlm46@gmail.com>
2019-10-29 19:22:30 +0100

    done

---
+++ index.js
@@ -0,0 +1,50 @@
+// CHANGING APPEARANCE ON H1 ELEMENT
+document.querySelector('h1').textContent = 'Fruits & Vegetables Corp';
+
+// CHANGING APPEARANCE ON LI ELEMENT
+const childNodeOfLi = document.querySelector('ul').lastElementChild.childNodes;
+
+for (let nodes of childNodeOfLi) {
+    nodes.textContent = ('Vegetables');
+    nodes.href = ('#vegetables');
+};
+
+// SWAPPING SECTION TAGS
+const main = document.getElementById('main');
+const about = document.getElementById('about');
+
+main.insertAdjacentElement('afterbegin', about);
+
+// CREATE NEW H2 ELEMENTS
+const contact = document.getElementById('contact');
+
+const elementAbout = document.createElement('H2');
+const elementContact = document.createElement('H2');
+elementAbout.innerHTML = 'About';
+elementContact.innerHTML = 'Contact';
+
+about.insertAdjacentElement("afterbegin", elementAbout);
+contact.insertAdjacentElement("afterbegin", elementContact);
+
+// WRAPPING TEXT IN A P-TAG
+const pElement = document.createElement('p');
+about.appendChild(pElement);
+pElement.appendChild(about.childNodes[1]);
+
+// CHANGE TD ELEMENTS TO TH INSIDE THEAD
+const tdElement = document.querySelectorAll('thead > tr > td');
+
+for (let nodes of tdElement) {
+    let thElement = document.createElement('TH');
+    thElement.innerHTML = nodes.innerHTML;
+    nodes.replaceWith(thElement);
+};
+
+// ADDING MAIN.CSS
+const addStylesheet = document.createElement('link');
+addStylesheet.rel = 'stylesheet';
+addStylesheet.href = 'main.css';
+document.querySelector('head').appendChild(addStylesheet);
+
+// CHANGING APPEARANCE IN HEAD TITLE
+document.querySelector('title').textContent = 'Fruits & Vegetables Corp';
